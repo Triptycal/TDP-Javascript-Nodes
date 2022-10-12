eb6a1fdf440638df81aeff743ae5015572d13032
Triptycal <36767994+Triptycal@users.noreply.github.com>
2022-10-12 11:59:22 +0100

    Create index.js

---
+++ Pets/index.js
@@ -0,0 +1,18 @@
+const express = require("express");
+
+const app = express ();
+
+const bodyParser = require("body-parser")
+
+app.use(bodyParser.json());
+
+const route = require(`./routes/pets.js`);
+
+app.use(route);
+
+
+app.use(express.json());
+
+const server = app.listen(1207,() => {console.log(`server has started succesfully on port number ${server.address().port}`)});
+
+module.exports=server
