81aaec7d3b1d3a92721b0ebb8a223929f344414b
Triptycal <36767994+Triptycal@users.noreply.github.com>
2022-10-19 13:48:11 +0100

    Add files via upload

---
+++ website/Routing/Users.jsx
@@ -0,0 +1,32 @@
+import {Link, useParams} from 'react-router-dom';
+import React from 'react';
+import {
+  BrowserRouter as Router, Route, Routes,
+} from 'react-router-dom';
+
+ const Users = () => {
+     const {id} = useParams(); 
+     return(
+         <>  
+             <h1>Users</h1>
+             <strong>Select a user:</strong>
+             <ul>
+                 <li>
+                     <Link to="/users/1">User 1</Link>
+                 </li>
+                 <li>
+                     <Link to="/users/2">User 2</Link>
+                 </li>
+                 <li>
+                     <Link to="/users/3">User 3</Link>
+                 </li>
+             </ul>
+             <p>{id}</p>
+             <Routes>
+          <Route path="/Users" element={<Users/>} />
+          <Route path="/Users/:id" element={<Users/>} />
+        </Routes>
+         </>
+     );
+ }
+ export default Users; 
